e7e7fdd6ba33ba1fe645f140b9de67c48ff197de
rigel <amagi6272@gmail.com>
2016-06-12 10:50:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# misc perl scripts
+# perl
---
+++ spreadsheet_test.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use Data::Printer;
+use Spreadsheet::ParseExcel;
+use Spreadsheet::ParseExcel::SaveParser;
+
+my $path = "";
+my $filename = "";
+my $worksheet_idx = "";
+    
+my $parser   = Spreadsheet::ParseExcel::SaveParser->new();
+my $template = $parser->Parse($path . $filename);
+
+# ---- TODO: Grab range, iterate, and update vals in cells
+# Change to select sheet based on name (typically "Mon day Invoice")
+my $worksheet = $template->worksheet($worksheet_idx); 
+my $row  = 15;
+my $col  = 0;
+
+# Overwrite the string in cell A1
+# ---- TODO: Don't hardcode val, get from user
+$worksheet->AddCell( $row, $col, '15' );
+
+# Write over the existing file or write a new file.
+$template->SaveAs($path . "new " . $filename );
+
+# ---- TODO: Formulas must be added to new file -- SaveParser cannot pull them from original file.
+
+#my $spreadsheet = $template->worksheet(20);
+#my $spreadsheet_name = $spreadsheet->get_name();
+#my $workbook = $template->SaveAs($path . "new " . $filename );
+#my $ws = $workbook->sheets(21);
+#$workbook->close();
